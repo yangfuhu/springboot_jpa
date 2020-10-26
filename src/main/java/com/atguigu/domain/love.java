460e63b39da95437c2015dc26578ae7723468331
yangfuhu <1693506265@qq.com>
2020-10-26 22:48:49 +0800

    增加了内容2222

--- src/main/java/com/atguigu/domain/love.java
+++ src/main/java/com/atguigu/domain/love.java
@@ -2,6 +2,6 @@ package com.atguigu.domain;
 
 public class love {
     public static void main(String[] args) {
-        System.out.println("11111111");
+        System.out.println("1111111122222");
     }
 }

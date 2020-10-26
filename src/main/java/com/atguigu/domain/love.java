956e9f4a5b9e91458fc78a2a670e1d12f2c128fa
yangfuhu <73372015+yangfuhu@users.noreply.github.com>
2020-10-26 23:21:13 +0800

    Update love.java

--- src/main/java/com/atguigu/domain/love.java
+++ src/main/java/com/atguigu/domain/love.java
@@ -5,5 +5,6 @@ public class love {
         System.out.println("1111111122222");
         System.out.println("1111111133333");
         System.out.println("有修改了代码");
+        System.out.println("测试拉取");
     }
 }

1b71a4f500cf45795d8af1248ef2f6c2fd1b34b0
yangfuhu <1693506265@qq.com>
2020-10-26 23:09:34 +0800

    联系push

--- src/main/java/com/atguigu/domain/love.java
+++ src/main/java/com/atguigu/domain/love.java
@@ -4,5 +4,6 @@ public class love {
     public static void main(String[] args) {
         System.out.println("1111111122222");
         System.out.println("1111111133333");
+        System.out.println("有修改了代码");
     }
 }
